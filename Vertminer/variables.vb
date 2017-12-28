0fba23af39df28a67452150c1444314c75b3b0a0
Zemrys <joe@vertcoin.org>
2017-12-28 12:35:18 -0500

    2nd network scanner port fix

--- Vertminer/variables.vb
+++ Vertminer/variables.vb
@@ -68,7 +68,7 @@ Module variables
     'API Variables
     Public Property api_connected As Boolean
     Public Property api_network1_hosts() = {"http://scanner1.alwayshashing.com:8085/api", "https://scanner.vtconline.org/api"}
-    Public Property api_network2_hosts() = {"http://scanner2.alwayshashing.com:8085/api"}
+    Public Property api_network2_hosts() = {"http://scanner2.alwayshashing.com:8086/api"}
     Public Property network1data As DataSet
     Public Property network2data As DataSet
 
